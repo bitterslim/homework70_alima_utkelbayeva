dec4d45b98c05196052fc0ab766f85a8c96edbbc
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-17 16:10:51 +0600

    Добвлены методы вывода авторизации

---
+++ accounts/views.py
@@ -0,0 +1,38 @@
+from django.contrib import messages
+from django.views.generic import TemplateView
+from accounts.forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form()
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            messages.error(request, "Incorrect login or password")
+            return redirect('index')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            messages.warning(request, "Username is not found")
+            return redirect('index')
+        login(request, user)
+        messages.success(request, 'Welcome')
+        next = request.GET.get('next')
+        if next:
+            return redirect(next)
+        return redirect('index')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
