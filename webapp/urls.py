e044180b5bdd1216c374fe11a311328301029022
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-17 16:07:02 +0600

    Добавлены ссылки страниц

---
+++ webapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from webapp.views.tasks import TaskDetailView, TaskAddView, TaskUpdateView, TaskDeleteView
+from webapp.views.base import IndexView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name = 'index'),
+    path('task/add/', TaskAddView.as_view(), name = 'task_add'),
+    path('task/<int:pk>', TaskDetailView.as_view(), name = 'task_detail'),
+    path('task/<int:pk>/edit', TaskUpdateView.as_view(), name = 'task_update'),
+    path('task/<int:pk>/delete', TaskDeleteView.as_view(), name='task_delete')
+]
