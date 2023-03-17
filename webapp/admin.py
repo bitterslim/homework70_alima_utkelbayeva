743ef8288c12eba6c466854f6ffaa8848ab50092
Alima Utkelbayeva <alima150201@gmail.com>
2023-03-17 16:05:34 +0600

    Модели зарегестрированы в админ панеле

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from webapp.models.task import Task
+from webapp.models.status import Status
+from webapp.models.type import Type
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id', 'status', 'created_at')
+    list_filter = ('id', 'title')
+    search_fields = ('title', 'description', 'type', 'created_at')
+    fields = ('title', 'description', 'status', 'type')
+
+admin.site.register(Task, TaskAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status')
+
+admin.site.register(Status, StatusAdmin)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'id')
+
+admin.site.register(Type, TypeAdmin)
